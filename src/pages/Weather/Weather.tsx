66fe52ad193a5b2f055e4adc33bbfa8660d2ae8c
arifmia1129 <arifmia1129@gmail.com>
2023-11-16 07:31:53 +0600

    [DONE] env variable setup

--- src/pages/Weather/Weather.tsx
+++ src/pages/Weather/Weather.tsx
@@ -1,9 +1,39 @@
+import { Select } from "antd";
 import "./Weather.css";
+import { useState } from "react";
+import axios from "axios";
 
 export default function Weather() {
+  const [searchTerm, setSearchTerm] = useState<string | null>(null);
+  const [availableLocations, setAvailableLocations] = useState([]);
+  console.log();
+
+  const handleSearchLocation = async () => {
+    const { data } = await axios.get();
+  };
+
+  const onChange = (value: string) => {
+    setSearchTerm(value);
+  };
+
+  const onSearch = (value: string) => {
+    console.log("search:", value);
+  };
   return (
     <div className="container">
-      <div>jnfskfsfkl</div>
+      <div style={{ maxWidth: "1280px" }}>
+        <Select
+          size="large"
+          showSearch
+          className="select-address"
+          placeholder="Select a person"
+          optionFilterProp="children"
+          onChange={onChange}
+          onSearch={onSearch}
+          // filterOption={filterOption}
+          options={availableLocations}
+        />
+      </div>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return defineConfig({
+    define: {
+      "process.env": env,
+    },
+    server: {
+      port: 3000,
+    },
+  });
+};
